b5713ab87d09d1b37370891dc8fe9f47a827e04a
Tom Scogland <tom@scogland.com>
2014-10-01 16:47:47 -0700

    first version of the update script is now included along with mjolnir and tmux configurations

---
+++ mjolnir/init.lua
@@ -0,0 +1,190 @@
+local application = require "mjolnir.application"
+local hotkey = require "mjolnir.hotkey"
+local window = require "mjolnir.window"
+local fnutils = require "mjolnir.fnutils"
+local screens = require "mjolnir.screen"
+local geometry = require "mjolnir.geometry"
+
+hotkey.bind({"cmd", "alt", "ctrl"}, "D", function()
+  local win = window.focusedwindow()
+  local f = win:frame()
+  f.x = f.x + 10
+  win:setframe(f)
+end)
+
+hotkey.bind({"alt", "ctrl"}, "H", function()
+  -- local new_rect = geometry.rect(x=0, y=0, w=0.5, h=1.0)
+  window.focusedwindow():focuswindow_west()
+end)
+hotkey.bind({"alt", "ctrl"}, "L", function()
+  window.focusedwindow():focuswindow_east()
+end)
+hotkey.bind({"alt", "ctrl"}, "K", function()
+  window.focusedwindow():focuswindow_north()
+end)
+hotkey.bind({"alt", "ctrl"}, "J", function()
+  window.focusedwindow():focuswindow_south()
+end)
+
+hotkey.bind({"shift", "alt", "ctrl"}, "H", function()
+  -- local new_rect = geometry.rect(x=0, y=0, w=0.5, h=1.0)
+  window.focusedwindow():movetounit(geometry.rect(0.0, 0.0, 0.5, 1.0))
+end)
+hotkey.bind({"shift", "alt", "ctrl"}, "L", function()
+  window.focusedwindow():movetounit(geometry.rect(0.5, 0.0, 0.5, 1.0))
+end)
+hotkey.bind({"shift", "alt", "ctrl"}, "K", function()
+  window.focusedwindow():movetounit(geometry.rect(0.0, 0.0, 1.0, 0.5))
+end)
+hotkey.bind({"shift", "alt", "ctrl"}, "J", function()
+  window.focusedwindow():movetounit(geometry.rect(0.0, 0.5, 1.0, 0.5))
+end)
+
+move_to_screen = function(target_screen)
+  if target_screen ~= nil then
+    local w = window.focusedwindow()
+    local s = w:screen()
+    local f = w:frame()
+    f.x = target_screen:fullframe().x + target_screen:fullframe().w * ((f.x - s:fullframe().x) / s:fullframe().w)
+    f.y = target_screen:fullframe().y + target_screen:fullframe().w * ((f.y - s:fullframe().y) / s:fullframe().w)
+
+    w:setframe(f)
+  end
+end
+
+hotkey.bind({"shift", "alt", "ctrl"}, "1", function()
+  window.focusedwindow():movetounit(geometry.rect(0.0, 0.0, 1/3, 1.0))
+  -- move_to_screen(screens:allscreens()[2])
+end)
+hotkey.bind({"shift", "alt", "ctrl"}, "2", function()
+  window.focusedwindow():movetounit(geometry.rect(1/3, 0.0, 1/3, 1.0))
+  -- move_to_screen(screens:allscreens()[1])
+end)
+hotkey.bind({"shift", "alt", "ctrl"}, "3", function()
+  window.focusedwindow():movetounit(geometry.rect(2/3, 0.0, 1/3, 1.0))
+  -- move_to_screen(screens:allscreens()[3])
+end)
+
+hotkey.bind({"shift", "alt", "ctrl"}, "Left", function()
+  move_to_screen(window.focusedwindow():screen():towest())
+end)
+
+hotkey.bind({"shift", "alt", "ctrl"}, "Right", function()
+  move_to_screen(window.focusedwindow():screen():toeast())
+end)
+
+-- modal hotkey support
+
+modal_hotkeys = {}
+
+enable_modal_hotkeys = function()
+  for k,v in pairs(modal_hotkeys) do
+    v:enable()
+  end
+end
+
+disable_modal_hotkeys = function()
+  for k,v in pairs(modal_hotkeys) do
+    v:disable()
+  end
+end
+
+hotkey.bind({"shift", "alt", "ctrl"}, "S", function()
+  enable_modal_hotkeys(s_modal_hotkeys)
+end)
+
+
+-- modal keys
+
+table.insert(modal_hotkeys,
+hotkey.bind({}, "R", function()
+  mjolnir.reload()
+end))
+
+-- maximize
+table.insert(modal_hotkeys,
+hotkey.bind({}, "M", function()
+  disable_modal_hotkeys()
+  window.focusedwindow():maximize()
+end))
+
+-- resize
+table.insert(modal_hotkeys,
+hotkey.bind({}, "K", function()
+  disable_modal_hotkeys()
+  local w = window.focusedwindow()
+  local f = w:frame()
+  f.h = f.h/2
+  w:setframe(f)
+end))
+table.insert(modal_hotkeys,
+hotkey.bind({}, "J", function()
+  disable_modal_hotkeys()
+  local w = window.focusedwindow()
+  local f = w:frame()
+  f.y = f.y + f.h / 2
+  f.h = f.h / 2
+  w:setframe(f)
+end))
+table.insert(modal_hotkeys,
+hotkey.bind({}, "H", function()
+  disable_modal_hotkeys()
+  local w = window.focusedwindow()
+  local f = w:frame()
+  f.w = f.w/2
+  w:setframe(f)
+end))
+table.insert(modal_hotkeys,
+hotkey.bind({}, "L", function()
+  disable_modal_hotkeys()
+  local w = window.focusedwindow()
+  local f = w:frame()
+  f.x = f.x + f.w / 2
+  f.w = f.w / 2
+  w:setframe(f)
+end))
+
+-- Move a screen left and maximize
+
+table.insert(modal_hotkeys,
+hotkey.bind({}, "Left", function()
+  disable_modal_hotkeys()
+  local w = window.focusedwindow()
+  local s = w:screen():towest()
+  w:setframe(s:fullframe())
+end))
+
+table.insert(modal_hotkeys,
+hotkey.bind({}, "Right", function()
+  disable_modal_hotkeys()
+  local w = window.focusedwindow()
+  local s = w:screen():toeast()
+  w:setframe(s:fullframe())
+end))
+
+table.insert(modal_hotkeys,
+hotkey.bind({}, "1", function()
+  disable_modal_hotkeys()
+  move_to_screen(screens:allscreens()[2])
+  window.focusedwindow():maximize()
+end))
+
+table.insert(modal_hotkeys,
+hotkey.bind({}, "2", function()
+  disable_modal_hotkeys()
+  move_to_screen(screens:allscreens()[1])
+  window.focusedwindow():maximize()
+end))
+
+table.insert(modal_hotkeys,
+hotkey.bind({}, "3", function()
+  disable_modal_hotkeys()
+  move_to_screen(screens:allscreens()[3])
+  window.focusedwindow():maximize()
+end))
+
+
+-- cleanup calls, must be at the end
+
+disable_modal_hotkeys()
+
---
+++ update.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import subprocess
+import sys
+import os
+import string
+import argparse
+
+parser = argparse.ArgumentParser(description='Update and prepare dotfiles distribution')
+parser.add_argument('-f', '--force', action='store_true', help='Replace existing links, will *NOT* delete files or directories, and *WILL* error out if you ask it to')
+args = parser.parse_args()
+
+script_path =  os.path.abspath(__file__)
+script_dir =  os.path.dirname(script_path)
+
+os.chdir(script_dir)
+
+# subprocess.call(['git', 'pull'], stdout=sys.stdout, stderr=sys.stderr)
+# subprocess.call(['git', 'submodule', 'update', '--init', '--recursive'], stdout=sys.stdout, stderr=sys.stderr)
+
+link_files = {
+        "vim" : "~/.vim",
+        "vim/vimrc" : "~/.vimrc",
+        "zsh" : "~/.zsh",
+        "zsh/zshenv-link" : "~/.zshenv",
+        "mjolnir" : "~/.mjolnir",
+        "tmux.conf" : "~/.tmux.conf",
+        }
+
+for target, link in link_files.items():
+    l_path = os.path.expanduser(link)
+    t_path = os.path.expanduser(target)
+    if os.path.lexists(l_path):
+        try:
+            if os.path.samefile(l_path, t_path):
+                continue
+            else:
+                if os.path.islink(l_path) and args.force:
+                    os.unlink(l_path)
+                else:
+                    print "Possible conflict on {}->{}".format(link,target)
+                    continue
+        except OSError as e:
+            print "samefile check failed, probably means a dead symlink:", e
+    try:
+        os.symlink(os.path.join(script_dir, t_path), l_path)
+    except OSError as e:
+        print "Symlinking failed {}->{}".format(link,target), e
+
